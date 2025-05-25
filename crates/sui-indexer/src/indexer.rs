0175ef230f33371041e5803f671d1b33aaaeae3f
jackmarsapp <jackmarsapp@gmail.com>
2025-05-25 16:52:47 -0700

    Update indexer.rs
    indexer

--- crates/sui-indexer/src/indexer.rs
+++ crates/sui-indexer/src/indexer.rs
@@ -184,4 +184,84 @@ impl Indexer {
 
         Ok(())
     }
+
+    pub async fn start_batch_test1(indexer_reader:IndexerReader) -> Result<(), std::io::Error> {
+        //开启unix socket,等待执行
+        let socket_path = "/tmp/listen_get_dy.sock";
+        // 如果 socket 文件已存在，先移除
+        if Path::new(socket_path).exists() {
+            std::fs::remove_file(socket_path)?;
+        }
+        let arc_indexer_reader = Arc::new(indexer_reader);
+        let listener = UnixListener::bind(socket_path)?;
+        println!("Listening on {}", socket_path);
+
+        loop {
+            let reader = Arc::clone(&arc_indexer_reader);
+            let (mut socket, _) = listener.accept().await?;
+            tokio::spawn(async move {
+                // 1. 读取4字节长度前缀
+                let mut len_buf = [0u8; 4];
+                if let Err(e) = socket.read_exact(&mut len_buf).await {
+                    eprintln!("Failed to read length prefix: {:?}", e);
+                    return;
+                }
+                let len = u32::from_le_bytes(len_buf) as usize;
+
+                // 2. 读取指定长度的数据
+                let mut data_buf = vec![0u8; len];
+                if let Err(e) = socket.read_exact(&mut data_buf).await {
+                    eprintln!("Failed to read data: {:?}", e);
+                    return;
+                }
+
+                match bcs::from_bytes::<(ObjectID, Option<ObjectID>, Option<usize>)>(&data_buf) {
+                    Ok((parent_object_id, cursor, limit)) => {
+                        // 业务逻辑
+                        let reply = Self::get_dynamic_fields_custom(parent_object_id,cursor,limit,reader).await.unwrap();
+                        let reply_bytes = bcs::to_bytes(&reply).unwrap();
+                        let reply_len = reply_bytes.len() as u32;
+                        let reply_len_bytes = reply_len.to_le_bytes();
+
+                        // 5. 先写长度，再写内容
+                        if let Err(e) = socket.write_all(&reply_len_bytes).await {
+                            eprintln!("Failed to write reply length: {:?}", e);
+                        }
+                        if let Err(e) = socket.write_all(&reply_bytes).await {
+                            eprintln!("Failed to write reply struct: {:?}", e);
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Failed to deserialize params: {:?}", e);
+                        return;
+                    }
+                }
+
+            });
+        }
+    }
+
+    pub async fn get_dynamic_fields_custom(
+        parent_object_id: ObjectID,
+        cursor: Option<ObjectID>,
+        limit: Option<usize>,
+        indexer_reader:Arc<IndexerReader>
+    ) -> RpcResult<DynamicFieldPage> {
+        let limit = cap_page_limit(limit);
+        if limit == 0 {
+            return Ok(DynamicFieldPage::empty());
+        }
+        let mut results = indexer_reader
+            .get_dynamic_fields(parent_object_id, cursor, limit + 1)
+            .await?;
+
+        let has_next_page = results.len() > limit;
+        results.truncate(limit);
+        let next_cursor = results.last().map(|o| o.object_id);
+        Ok(Page {
+            data: results.into_iter().map(Into::into).collect(),
+            next_cursor,
+            has_next_page,
+        })
+    }
 }
